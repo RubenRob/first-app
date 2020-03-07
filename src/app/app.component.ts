dbe5acacedafe88645cf99c3b14fae687f5562db
RubenRob <ruben_rob@hotmail.com>
2020-03-07 13:47:45 +0100

    met timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,52 +2,77 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { NgIf } from '@angular/common';
 
-@Component({
+
+import { Injectable} from  '@angular/core';
+import { Subject} from  'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+  @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'firstApp';
-  getal;  
   randomGetal;
+  timer;
+  getal;  
+  lijst;    
   teller;
   messageGetal;
   messageTeller;
+  buttonOpnieuw;
+
+  secondsSubject: Subject<number> = new Subject<number>();
+  seconds = this.secondsSubject.asObservable();
 
   constructor() {
     this.Initialize();
+
+    setInterval(() => {
+      this.timer -= 1;
+      this.secondsSubject.next(this.teller);
+    }, 1000);
   }
 
   Initialize(){    
     this.randomGetal = Math.floor(Math.random() * 100) + 1
-    this.teller = 2;
-    window.alert(this.randomGetal);
+    this.teller = 3;
+    this.timer = 60;
+    this.lijst = "";
+    this.buttonOpnieuw = false;
   }
 
   
   DoeEenGok() {
-    /*als this.randomGetal = getal --> joepie
-    anders is de gok groter of kleiner
-    */
+
+    this.teller = this.teller -1
+    this.messageTeller="U heeft nog" + this.teller + "kansen."
+    this.lijst = this.getal + " "  + this.lijst
    if(this.getal == this.randomGetal)
    {
-      this.messageGetal = "u hebt het getal geraden! Proficiat."
+      this.messageGetal = "U hebt het getal geraden! Proficiat."
+      this.messageTeller=""
+      this.buttonOpnieuw = true;
    }
    else
    {
      if(this.getal < this.randomGetal)
-        this.messageGetal = "Uw gekozen getal is te klein"
+        this.messageGetal = "Uw gekozen getal is te klein, u probeerde reeds " + this.lijst + "."
       else
-        this.messageGetal = "Uw gekozen getal is te groot"
+        this.messageGetal = "Uw gekozen getal is te groot, u probeerde reeds " + this.lijst + "."
     }
-    this.teller = this.teller -1
     this.getal = null;
-    this.messageTeller="u heeft nog" + this.teller + "kansen."
     if (this.teller == 0)
     {
-      this.messageGetal = "u hebt verloren."
-      this.Initialize();
+      this.messageGetal = "U heeft verloren, het getal was " + this.randomGetal + "."
+      this.messageTeller=""
+      this.buttonOpnieuw = true;
     }
 
   }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-    imports: [MatButtonModule,],
-    exports: [MatButtonModule,]
+    imports: [FormsModule, MatButtonModule, MatInputModule,],
+    exports: [FormsModule, MatButtonModule, MatInputModule,]
 })
 
 export class MaterialModule {}
